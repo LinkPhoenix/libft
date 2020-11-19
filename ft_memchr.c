8a0e31cfc282fe67891d6a5d5ab7f1907128d77e
LinkPhoenix <linkblue11@outlook.com>
2020-11-19 16:47:14 +0100

    add memmove and memchr

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/19 16:19:32 by emlecerf          #+#    #+#             */
+/*   Updated: 2020/11/19 16:46:31 by emlecerf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const char		*s_cpy;
+	unsigned char	c_cpy;
+	size_t			i;
+
+	s_cpy = (const char *)s;
+	c_cpy = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (s_cpy[i] == c_cpy)
+			return ((void *)s_cpy + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_memmove.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/19 09:44:39 by emlecerf          #+#    #+#             */
+/*   Updated: 2020/11/19 16:16:17 by emlecerf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char		*dst_cpy;
+	const char	*src_cpy;
+
+	dst_cpy = (char *)dst;
+	src_cpy = (const char *)src;
+	if (dst_cpy > src_cpy)
+	{
+		while (len-- > 0)
+			dst_cpy[len] = src_cpy[len];
+	}
+	else
+	{
+		while (len-- > 0)
+			*dst_cpy++ = *src_cpy++;
+	}
+	return (dst);
+}
