bc480b73cac98487f429a1959e36e458ba3b560d
LinkPhoenix <linkblue11@outlook.com>
2020-11-20 13:25:56 +0100

    add strdup

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/20 12:40:08 by emlecerf          #+#    #+#             */
+/*   Updated: 2020/11/20 13:23:39 by emlecerf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s1_cpy;
+	int		i;
+
+	i = 0;
+	if (!(s1_cpy = malloc(sizeof(char) * ft_strlen(s1) + 1)))
+		return (NULL);
+	while (s1_cpy[i])
+	{
+		s1_cpy[i] = s1[i];
+		i++;
+	}
+	s1_cpy[i] = '\0';
+	return (s1_cpy);
+}
