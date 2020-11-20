cda93bee795006beaae475746d2f5d9c097ff4c8
LinkPhoenix <linkblue11@outlook.com>
2020-11-20 12:36:59 +0100

    add calloc

---
+++ ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/20 11:06:45 by emlecerf          #+#    #+#             */
+/*   Updated: 2020/11/20 12:35:53 by emlecerf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr
+
+	if (!( ptr = malloc(count * size))
+			return (NULL);
+	ft_bzero(ptr, count * size)
+	return (ptr)
+}
