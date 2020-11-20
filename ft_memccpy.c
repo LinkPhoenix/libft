eb6ca0bf3d561b904a2a03441aa0f8fc996072f2
LinkPhoenix <linkblue11@outlook.com>
2020-11-20 21:23:51 +0100

    fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 21:02:48 by emlecerf          #+#    #+#             */
-/*   Updated: 2020/11/20 21:15:15 by emlecerf         ###   ########.fr       */
+/*   Updated: 2020/11/20 21:23:39 by emlecerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	char				*dest_cpy;
+	unsigned char		*dest_cpy;
 	unsigned const char	*src_cpy;
 	size_t				i;
 
-	dest_cpy = (char *)dest;
+	dest_cpy = (unsigned char *)dest;
 	src_cpy = (unsigned const char *)src;
 	i = 0;
 	while (i < n)
 	{
 		dest_cpy[i] = src_cpy[i];
-		if ((unsigned char)src_cpy == (unsigned char)c)
+		if ((src_cpy == (unsigned char)c)
 			return (&dest_cpy[i + 1]);
 		i++;
 	}
