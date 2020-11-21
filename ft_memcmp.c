c44196214baf85cd448f3b647e074a93ef35115c
LinkPhoenix <linkblue11@outlook.com>
2020-11-21 01:25:29 +0100

    fix memcmp cast unsigned const char

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 16:50:24 by emlecerf          #+#    #+#             */
-/*   Updated: 2020/11/20 11:03:19 by emlecerf         ###   ########.fr       */
+/*   Updated: 2020/11/21 01:23:48 by emlecerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char *s1_cpy;
-	const char *s2_cpy;
+	unsigned const char	*s1_cpy;
+	unsigned const char	*s2_cpy;
 
-	s1_cpy = (const char *)s1;
-	s2_cpy = (const char *)s2;
+	s1_cpy = (unsigned const char *)s1;
+	s2_cpy = (unsigned const char *)s2;
 	while ((*s1_cpy || *s2_cpy) && n > 0)
 	{
 		if (*s1_cpy != *s2_cpy)
-			return (s1_cpy - s2_cpy);
+			return (*s1_cpy - *s2_cpy);
 		s1_cpy++;
 		s2_cpy++;
 		n--;
