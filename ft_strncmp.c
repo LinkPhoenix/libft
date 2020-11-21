9cd9beef064a901ab9bbb56cf2d7395bd6f694fb
LinkPhoenix <linkblue11@outlook.com>
2020-11-21 02:14:33 +0100

    fix strncmp | cast unsigned const char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:27:57 by emlecerf          #+#    #+#             */
-/*   Updated: 2020/11/18 16:59:55 by emlecerf         ###   ########.fr       */
+/*   Updated: 2020/11/21 02:13:53 by emlecerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	unsigned const char	*s1_cpy;
+	unsigned const char *s2_cpy;
+	size_t				i;
 
+	s1_cpy = (unsigned const char *)s1;
+	s2_cpy = (unsigned const char *)s2;
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((int)(s1_cpy[i] - s2_cpy[i]));
 		i++;
 	}
 	return (0);
