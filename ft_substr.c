f3d24ff253dce693ddbb63d443a2702ef721718c
LinkPhoenix <linkblue11@outlook.com>
2020-11-27 12:47:03 +0100

    Fix : substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 13:50:55 by emlecerf          #+#    #+#             */
-/*   Updated: 2020/11/26 03:03:11 by emlecerf         ###   ########.fr       */
+/*   Updated: 2020/11/27 12:45:57 by emlecerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	end = ft_strlen(s);
+	if (!s)
+		return (NULL);
 	if (!(tab = (char *)malloc(sizeof(*s) * len + 1)))
 		return (NULL);
-	while (len-- > 0 || i++ == end)
+	tab[i] = '\0';
+	if (start >= end)
+		return (tab);
+	while (len-- > 0)
 		tab[i++] = s[start++];
-	tab[end] = 0;
+	tab[i] = '\0';
 	return (tab);
 }
