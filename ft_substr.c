b4f63bc9ec3cce5851ede78141dbd908175eeb83
LinkPhoenix <linkblue11@outlook.com>
2020-11-27 17:29:02 +0100

    PROTECT : substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 13:50:55 by emlecerf          #+#    #+#             */
-/*   Updated: 2020/11/27 12:45:57 by emlecerf         ###   ########.fr       */
+/*   Updated: 2020/11/27 17:28:36 by emlecerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	end = ft_strlen(s);
 	if (!s)
 		return (NULL);
+	end = ft_strlen(s);
 	if (!(tab = (char *)malloc(sizeof(*s) * len + 1)))
 		return (NULL);
 	tab[i] = '\0';
