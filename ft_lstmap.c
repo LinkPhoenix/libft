3bd48311eaff45037047d15d71ee6147d602c037
LinkPhoenix <linkblue11@outlook.com>
2020-11-28 20:09:15 +0100

    FIX : lstmap protect affectation

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,18 +14,27 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_element;
 	t_list	*new_list;
-	void	*issucces;
+	void	*tmp;
 
+	if (!f)
+		return (NULL);
 	new_list = NULL;
-	while (lst && f)
+	while (lst)
 	{
-		if (!(issucces = f(lst->content)))
+		if (!(tmp = f(lst->content)))
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, ft_lstnew(issucces));
+		if (!(new_element = ft_lstnew(tmp)))
+		{
+			ft_lstclear(&new_list, del);
+			del(tmp);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_element);
 		lst = lst->next;
 	}
 	return (new_list);
