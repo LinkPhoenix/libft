5ca5e37bc35f9e89d76fa21c1f0aece3c40d79d2
LinkPhoenix <linkblue11@outlook.com>
2020-11-30 01:06:29 +0100

    FIX : strchr use ascii extended

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,16 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return ((char *)s + i);
-		i++;
-	}
-	if (s[i] == c)
-		return ((char *)s + i);
+	while (*s && (unsigned char)*s != (unsigned char)c)
+		s++;
+	if ((unsigned char)*s == (unsigned char)c)
+		return ((char *)s);
 	return (NULL);
 }
