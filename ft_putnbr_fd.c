329bad4525d97f704dfcb8d20aa8a9a557966f63
LinkPhoenix <linkblue11@outlook.com>
2020-11-30 15:55:02 +0100

    BETTER : punbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,27 +6,36 @@
 /*   By: emlecerf <emlecerf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 23:07:29 by emlecerf          #+#    #+#             */
-/*   Updated: 2020/11/23 23:52:06 by emlecerf         ###   ########.fr       */
+/*   Updated: 2020/11/30 15:30:58 by emlecerf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
-	long number;
+	long	nbr;
+	long	i;
+	char	nbr_char[11];
 
-	number = n;
-	if (number < 0)
+	nbr = nb;
+	i = 10;
+	nbr_char[10] = 48;
+	if (nb == -2147483648)
 	{
-		ft_putchar_fd('-', fd);
-		number *= -1;
+		nbr_char[i--] = '8';
+		nb /= 10;
 	}
-	if (number < 10)
-		ft_putchar_fd(number + 48, fd);
-	else
+	if (nb < 0)
+		nb *= -1;
+	while (nb > 0)
 	{
-		ft_putnbr_fd(number / 10, fd);
-		ft_putnbr_fd(number % 10, fd);
+		nbr_char[i--] = nb % 10 + 48;
+		nb /= 10;
 	}
+	if (nbr < 0)
+		nbr_char[i] = '-';
+	else if (nbr != 0)
+		i++;
+	write(fd, &nbr_char[i], 11 - i);
 }
